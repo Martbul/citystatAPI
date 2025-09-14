2b9df16cac407766b2ba8238f14a1915ed31fc25
Martbul <martbul01@gmail.com>
2025-09-14 13:34:28 +0300

    fix: userupdatereq struct

--- types/user.go
+++ types/user.go
@@ -32,25 +32,15 @@ type CityData struct {
 // 	} `json:"cityCoords,omitempty"` 
     // }
 
-    type UserUpdateRequest struct {
-        FirstName                 string `json:"firstName,omitempty"`
-        LastName                  string `json:"lastName,omitempty"`
-        UserName                  string `json:"userName,omitempty"`
-        ImageURL                  string `json:"imageUrl,omitempty"`
-        CompletedTutorial         bool   `json:"completedTutorial,omitempty"`
-        IsLocationTrackingEnabled bool   `json:"isLocationTrackingEnabled,omitempty"`
-        // City selection data
-        SelectedCity *CityData `json:"selectedCity,omitempty"`
-        // Alternative: if you prefer individual fields
-        CityName    string `json:"cityName,omitempty"`
-        CityCountry string `json:"cityCountry,omitempty"`
-        CityState   string `json:"cityState,omitempty"`
-        CityCoords  *struct {
-            Lat float64 `json:"lat"`
-            Lng float64 `json:"lng"`
-        } `json:"cityCoords,omitempty"`
-    }
-
+type UserUpdateRequest struct {
+    FirstName                 string    `json:"firstName,omitempty"`
+    LastName                  string    `json:"lastName,omitempty"`
+    UserName                  string    `json:"userName,omitempty"`
+    ImageURL                  string    `json:"imageUrl,omitempty"`
+    CompletedTutorial         bool      `json:"completedTutorial,omitempty"`
+    IsLocationTrackingEnabled bool      `json:"isLocationTrackingEnabled,omitempty"`
+    SelectedCity              *CityData `json:"selectedCity,omitempty"`
+}
 //TODO: Finish the profile req type and other logi for it
 type UserEditProfileRequest struct {
 	FirstName *string `json:"firstName,omitempty"`
